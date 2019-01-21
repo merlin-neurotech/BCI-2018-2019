e4d0f3f3a73553660e48633d4a1ccfcd82fd2b9f
Shawn Carere <shawncarere@gmail.com>
2019-01-21 10:51:44 -0500

    apparently i made some edits to bad n tag

--- Data Preprocessing/bagntag.py
+++ Data Preprocessing/bagntag.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 for patient in range(0,10):
-    data1 = pd.readcsv("Data/Patient", patient, "2019-01-15_TimeSeries_Muse-EEG_0.csv")
+    data1 = pd.read_csv("Data/Patient" + str(patient) + "/2019-01-15_TimeSeries_Muse-EEG_0.csv")
     print("Data/Patient" + str(patient) + "/2019-01-15_TimeSeries_Muse-EEG_0.csv")
-    AllData[i] = data1
-    AllData[]
+    #AllData[i] = data1
+    #AllData[]
