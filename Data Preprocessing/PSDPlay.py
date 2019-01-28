34104e2aaeed5afcfcd3eb802ce103969cbd72ad
Shawn Carere <37197312+scarere@users.noreply.github.com>
2019-01-27 21:12:52 -0500

    Update PSDPlay.py
    Added comments and resolves merge issues

--- Data Preprocessing/PSDPlay.py
+++ Data Preprocessing/PSDPlay.py
@@ -5,40 +5,27 @@ import numpy as np
 <<<<<<< HEAD
 spectrogramData = []
 PSDData = []
-fs = 256  #Sampling frequency. Calculate manually
+fs = 256  #Sampling frequency. Calculated manually
 
 
 for i in range(1,8):
-    electrodeData = pd.read_csv("MyData/TimeSeries"+str(i)+"_Muse-EEG_0.csv")
-    electrodeData = electrodeData.values
+    electrodeData = pd.read_csv("MyData/TimeSeries"+str(i)+"_Muse-EEG_0.csv") #Read data from csv
+    electrodeData = electrodeData.values #convert pandas dataframe to numpy array
     spectSample = []
     psdSample = []
     print(i)
     for k in range(1,6):
-        f1, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40)
-        f2, PSD = signal.welch(x=electrodeData[:,], fs=fs)
-        print(np.shape(sxx))
+        f1, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40) #calculate spectrogram data
+        f2, PSD = signal.welch(x=electrodeData[:,], fs=fs) #calculate PSD
+        #Append data for electrode to overall sample with 5 electrodes
         spectSample.append(sxx)
         psdSample.append(PSD)
+    #Append samples to transformed datasets
     spectrogramData.append(spectSample)
     PSDData.append(psdSample)
 
 print(np.shape(spectrogramData))
 print(np.shape(PSDData))
-=======
-data = []
-fs = 256  #Sampling frequency. Calculated manually
 
-
-for i in range(1,8):
-    electrodeData = pd.read_csv("MyData/TimeSeries"+str(i)+"_Muse-EEG_0.csv") #read csv data file
-    electrodeData = electrodeData.values #converts pandas data frame into numpy array
-    sample = []
-    print(i)
-    for k in range(1,6):
-        f, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40) #calculate spectrogram
-        sample.append(sxx)
-    data.append(sample)
->>>>>>> cbdcfb6f0c4c47397ce89879dd460a3714a397a8
 
 
