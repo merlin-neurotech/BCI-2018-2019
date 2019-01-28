e2095ec00005a2ea53e1979f343aa186c5c2605a
Shawn Carere <shawncarere@gmail.com>
2019-01-27 21:07:02 -0500

    Changed file to produced overall PSD and spectrogram transform data

--- Data Preprocessing/PSDPlay.py
+++ Data Preprocessing/PSDPlay.py
@@ -2,20 +2,27 @@ from scipy import signal
 import pandas as pd
 import numpy as np
 
-data = []
+spectrogramData = []
+PSDData = []
 fs = 256  #Sampling frequency. Calculate manually
 
 
 for i in range(1,8):
     electrodeData = pd.read_csv("MyData/TimeSeries"+str(i)+"_Muse-EEG_0.csv")
     electrodeData = electrodeData.values
-    sample = []
+    spectSample = []
+    psdSample = []
     print(i)
     for k in range(1,6):
-        f, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40)
+        f1, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40)
+        f2, PSD = signal.welch(x=electrodeData[:,], fs=fs)
         print(np.shape(sxx))
-        sample.append(sxx)
+        spectSample.append(sxx)
+        psdSample.append(PSD)
+    spectrogramData.append(spectSample)
+    PSDData.append(psdSample)
 
-    data.append(sample)
+print(np.shape(spectrogramData))
+print(np.shape(PSDData))
 
 
