cbdcfb6f0c4c47397ce89879dd460a3714a397a8
Shawn Carere <37197312+scarere@users.noreply.github.com>
2019-01-27 21:02:04 -0500

    Update PSDPlay.py

--- Data Preprocessing/PSDPlay.py
+++ Data Preprocessing/PSDPlay.py
@@ -7,15 +7,13 @@ fs = 256  #Sampling frequency. Calculated manually
 
 
 for i in range(1,8):
-    electrodeData = pd.read_csv("MyData/TimeSeries"+str(i)+"_Muse-EEG_0.csv")
-    electrodeData = electrodeData.values
+    electrodeData = pd.read_csv("MyData/TimeSeries"+str(i)+"_Muse-EEG_0.csv") #read csv data file
+    electrodeData = electrodeData.values #converts pandas data frame into numpy array
     sample = []
     print(i)
     for k in range(1,6):
-        f, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40)
-        print(np.shape(sxx))
+        f, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40) #calculate spectrogram
         sample.append(sxx)
-
     data.append(sample)
 
 
