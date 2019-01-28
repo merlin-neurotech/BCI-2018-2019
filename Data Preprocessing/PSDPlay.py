3cf7c9c79bc7d539ea6ec6e8949e0f83bc4354d9
Shawn Carere <shawncarere@gmail.com>
2019-01-27 20:57:08 -0500

    Using Scipy to transform data into spectrogram

--- Data Preprocessing/PSDPlay.py
+++ Data Preprocessing/PSDPlay.py
@@ -1,31 +1,21 @@
-import wizardhat.transform as transform
+from scipy import signal
 import pandas as pd
 import numpy as np
 
 data = []
+fs = 256  #Sampling frequency. Calculate manually
 
-def get_windowed(data, n_fft):
-    data_centered = data - np.mean(data, axis=0)
-    window = np.hamming
-    window = window(n_fft).reshape((n_fft, 1))
-    data_windowed = data_centered * window
-    return data_windowed
-
-
-def getPSD(data, n_fft):
-    data_windowed = get_windowed(data, n_fft)
-    data_fft = np.fft.rfft(data_windowed, n=self.n_fft, axis=0)
-    data_fft /= n_fft
-    psd = 2 * np.abs(data_fft)
-    return psd
 
 for i in range(1,8):
-    holder = pd.read_csv("MyData/TimeSeries" + str(i) + "_Muse-EEG_0.csv")
-    fft = []
-    for t in range(0, len(holder)):
-      fft.append(getPSD(holder[None,i], len(holder)))
-data.append(fft)
-print(data)
+    electrodeData = pd.read_csv("MyData/TimeSeries"+str(i)+"_Muse-EEG_0.csv")
+    electrodeData = electrodeData.values
+    sample = []
+    print(i)
+    for k in range(1,6):
+        f, t, sxx = signal.spectrogram(x=electrodeData[:,k], fs=fs, nperseg=50, noverlap=40)
+        print(np.shape(sxx))
+        sample.append(sxx)
 
+    data.append(sample)
 
 
