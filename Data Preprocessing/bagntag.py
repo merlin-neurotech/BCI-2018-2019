b4d3817020137fde032677d78fb7db8c6d07da84
Shawn Carere <shawncarere@gmail.com>
2019-03-15 17:16:51 -0400

    test

--- Data Preprocessing/bagntag.py
+++ Data Preprocessing/bagntag.py
@@ -3,20 +3,6 @@ import os
 import numpy as np
 import csv
 
-electrodeData = []
-
-# for patient in range(1, 4):
-#     data = pd.read_csv("Muse Data/p"+str(patient)+"_f_EEG.csv") #Read data from csv
-#     #data = data.values #convert pandas dataframe to numpy array
-#     electrodeData.append(data)
-#     date = pd.read_csv("Muse Data/p" + str(patient) + "_r_EEG.csv")  # Read data from csv
-#     #data = data.values  # convert pandas dataframe to numpy array
-#     electrodeData.append(data)
-#
-# electrodeData = pd.DataFrame(electrodeData)
-# electrodeData.to_csv('electrodeData.csv')
-
-
 def bag_and_tag(num_of_patients): #shrey wrote this
     matrices = []
     labels = []
