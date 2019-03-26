15643bcada646eacb34692696f447478b3b54efe
Shawn Carere <shawncarere@gmail.com>
2019-03-26 15:47:52 -0400

    Shrey and I made changes to data importation

--- Data Preprocessing/bagntag.py
+++ Data Preprocessing/bagntag.py
@@ -2,6 +2,23 @@ import pandas as pd
 import os
 import numpy as np
 import csv
+from scipy import signal
+import matplotlib.pyplot as plt
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = signal.butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = signal.lfilter(b, a, data)
+    return y
+
 
 def bag_and_tag(num_of_patients): #shrey wrote this
     matrices = []
@@ -13,11 +30,22 @@ def bag_and_tag(num_of_patients): #shrey wrote this
             r_data = pd.read_csv("Muse Data/p" + str(num_of_patients)+"_r_EEG.csv")
             f_data.drop(f_data.columns[[0, 4]], axis=1, inplace=True)
             r_data.drop(r_data.columns[[0, 4]], axis=1, inplace=True)
+            f_data = f_data.transpose()
+            r_data = r_data.transpose()
             labels.append(1)
         # 1 is focused, 0 is relaxed
             labels.append(0)
             matrices.append(f_data)
             matrices.append(  r_data)
+    return matrices
+
+electrodeData = bag_and_tag(3)
+f1, t, sxx = signal.spectrogram(x=electrodeData[0], fs=256, nperseg=50, noverlap=20) #calculate spectrogram data
+filt = butter_bandpass_filter(sxx[0,:,0], 2, 30, 256, 10)
+#26 frequencies windows
+plt.plot(f1, sxx[0,:,0])
+plt.show()
+
 
 # def main():
 #     bag_and_tag(3)
