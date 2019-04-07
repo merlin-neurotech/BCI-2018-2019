632d9a901b84f57b9ae6132635f30116882ab0f1
Shawn Carere <shawncarere@gmail.com>
2019-04-07 16:47:33 -0400

    added k fold cross validation

--- Neural Networks/Network.py
+++ Neural Networks/Network.py
@@ -12,9 +12,24 @@ def buildModel(traindata):
 
     return model
 
+#Were going to use k-fold cross validation bc we don't have enough data for a validation set
+k = 4
+num_val_samples = len(traindata)//k #double slash rounds to nearest whole number
+num_epochs = 4
 
-#import and format data
-model = buildModel(traindata)
-history = model.fit(trainx, trainy, epochs=10, batch_size=32, validation_data=(xval, yval))
-results =  model.evaluate(x_test, y_test)
-print(results)
+for i in range(k):
+    print("Processing fold #", i)
+    #select current validation set
+    xval = traindata[i*num_val_samples: (i+1)*num_val_samples]
+    yval = trainlabels[i*num_val_samples: (i+1)*num_val_samples]
+    #cut out current validation set
+    xdata = np.concatenate([traindata[:i*num_val_samples],traindata[(i+1)*num_val_samples:]], axis=0)
+    ydata = np.concatenate([trainlabels[:i*num_val_samples],trainlabels[(i+1)*num_val_samples:]], axis=0)
+    kmodel = buildModel(traindata)
+    history = kmodel.fit(x=xdata, y=ydata, epochs=num_epochs, batch_size=1, verbose=2, validation_data=(xval, yval))
+    print(history.history.keys())
+    acc_hist = history.history["accuracy"]
+    all_acc_hist.append(acc_hist)
+    avg_acc = [np.mean([x[i] for x in all_acc_hist]) for i in range(num_epochs)]
+    #val_mse, val_mae = kmodel.evaluate(xval, yval, verbose=0)
+    #all_scores.append(val_mae)
